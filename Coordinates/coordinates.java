46abae8eb93838c5c9e2652d1e52be3d0e99548d
Ashu <138532112+ashutoshayn@users.noreply.github.com>
2025-06-16 09:57:36 +1200

    Add files via upload

---
+++ Coordinates/coordinates.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class coordinates here.
+ *
+ * @author Ashu
+ * @version (a version number or a date)
+ */
+public class coordinates
+{
+    public static void main(String[] args){
+        //the beginning
+    }
+}
---
+++ Coordinates/deck.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class deck here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class deck
+{
+    //this is deck
+}
